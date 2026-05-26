93382a05c71f0f3a4acbc26b83e56fb594088b33
doctorpangloss <2229300+doctorpangloss@users.noreply.github.com>
2026-05-25 20:46:17 -0700

    Install LMCache dependencies in Ampere images

--- tools/ampere/build_vllm_ampere_image.sh
+++ tools/ampere/build_vllm_ampere_image.sh
@@ -41,6 +41,7 @@ sccache_region="${SCCACHE_REGION_NAME:-us-west-2}"
 sccache_s3_no_credentials="${SCCACHE_S3_NO_CREDENTIALS:-0}"
 sccache_recache="${SCCACHE_RECACHE:-0}"
 skip_flash_attn_build="${VLLM_SKIP_FLASH_ATTN_BUILD:-0}"
+install_kv_connectors="${INSTALL_KV_CONNECTORS:-false}"
 
 secret_args=()
 cache_args=()
@@ -101,6 +102,7 @@ docker buildx build "${repo_root}" \
   --build-arg "SCCACHE_S3_NO_CREDENTIALS=${sccache_s3_no_credentials}" \
   --build-arg "SCCACHE_RECACHE=${sccache_recache}" \
   --build-arg "VLLM_SKIP_FLASH_ATTN_BUILD=${skip_flash_attn_build}" \
+  --build-arg "INSTALL_KV_CONNECTORS=${install_kv_connectors}" \
   --build-arg "VLLM_BUILD_COMMIT=${commit}" \
   --build-arg "VLLM_IMAGE_TAG=${tag}" \
   "${secret_args[@]}" \
